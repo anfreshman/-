ecf9b0b492693e85168014ccbdfc6d12280aa6ec
anfreshman <44454249+anfreshman@users.noreply.github.com>
2019-06-20 17:13:13 +0800

    Create mysearch.h

---
+++ HRADWARE/mysearch.h
@@ -0,0 +1,17 @@
+#ifndef __MYSEARCH_H
+#define __MYSEARCH_H
+#include "mybuild.h"
+
+
+int mySearch(unsigned long int number);
+
+int mySearch_class(int Class);
+
+int mychange(unsigned long int number);
+
+
+
+
+
+
+#endif
